0c8c3adf267e4a6f1f7c37ff2ebaaa05f46613d8
Liam Andrew <liam.andrew@fireant.com.au>
2026-06-02 15:03:26 +1000

    feat: update switch to use primary color

--- packages/tetra-ui/src/components/switch.tsx
+++ packages/tetra-ui/src/components/switch.tsx
@@ -3,16 +3,18 @@ import {
   Switch as SwitchPrimitive,
   type SwitchProps as SwitchPrimitiveProps,
 } from "@expo/ui";
+import { tint } from "@expo/ui/swift-ui/modifiers";
+import { useCSSVariable } from "uniwind";
 
 // Types
 type SwitchProps = Omit<SwitchPrimitiveProps, "modifiers">;
 
 // Components
-
 export const Switch = (props: SwitchProps) => {
+  const primaryColor = useCSSVariable("--color-primary") as string;
   return (
     <HostPrimitive matchContents>
-      <SwitchPrimitive {...props} />
+      <SwitchPrimitive {...props} modifiers={[tint(primaryColor)]} />
     </HostPrimitive>
   );
 };
